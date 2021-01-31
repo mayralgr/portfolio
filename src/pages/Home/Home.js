a00f2e9c3a617884e933fda84394a13969f9da3e
mayralgr <ml17ft@gmail.com>
2021-01-31 16:37:07 -0600

    Change navbar icons and fix routes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,8 +8,41 @@ const HomePage = () => {
             <h1>
                 Mayra Lucero Garcia Ramirez
             </h1>
+            <h3>
             Developer
+            </h3>
+
+
+            <h4>Hi thanks for reaching out!</h4>
+
+            <div className={styles.overview}>
+                <p>
+                    Let me tell you a little bit about myself 👀
+                    <br/>
+                    I am a proud mexican 🇲🇽, open (and love) to learn about cultures <br/>
+                    I lived one year in France (I speak french too) 🇫🇷 <br/>
+                    I love movies, music and sports.  <br/>
+                    I play football and basketball and I also play a little videogames ⚽️🏀🎮 <br/>
+                    <h4> But mostly love programming </h4>
+                    <br/>
+                    I am always doing things, I love to learn about new techonlogies and start using them.
+                    <br/>
+                    I studied Computer Engenierring at UASLP (in San Luis Potosí, Mexico) 📚<br/>
+                    with a double Degree in Aplied Computing (vía the UASLP-City University of Seattle)🇺🇸 <br/>
+                    did a year abroad in Lyon, France (beautiful city) at the École Centrale de Lyon 🖥 <br/>
+
+                    I did my internship at Encora.Inc in 2020
+                    <br/>
+                    Right now I am working for Nagarro (see info in linkedIn)
+                    <br/>
+                    <h4>Don't hesitate to contact me as this page is still under construction but the links in the menu work 😊</h4>
+                </p>
             
+            </div>
+            <footer>
+                Happy programming!!!!
+            </footer>
+
 
         </>
     )
