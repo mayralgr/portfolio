a54f6ffc7775275b43fa3a29a4acc566c1783622
mayralgr <ml17ft@gmail.com>
2022-03-05 13:38:53 -0600

    Updates

---
+++ precache-manifest.e2ec8b36e38e9660084723d09c2ebb5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdb7e36c052c7c88abc5",
+    "url": "/portfolio/static/css/main.1a9cdbfa.chunk.css"
+  },
+  {
+    "revision": "bdb7e36c052c7c88abc5",
+    "url": "/portfolio/static/js/main.bdb7e36c.chunk.js"
+  },
+  {
+    "revision": "b087d70075fbc48ee96a",
+    "url": "/portfolio/static/js/1.b087d700.chunk.js"
+  },
+  {
+    "revision": "8e5de9f6f941a2ccb313",
+    "url": "/portfolio/static/css/2.410fd347.chunk.css"
+  },
+  {
+    "revision": "8e5de9f6f941a2ccb313",
+    "url": "/portfolio/static/js/2.8e5de9f6.chunk.js"
+  },
+  {
+    "revision": "f6837e89a88ba22bcde8",
+    "url": "/portfolio/static/js/runtime~main.f6837e89.js"
+  },
+  {
+    "revision": "f60435e9dc85d2411b7bb39745fee882",
+    "url": "/portfolio/static/media/twitter.f60435e9.svg"
+  },
+  {
+    "revision": "7a1f185299c8daafa6f3df7ab3efacb0",
+    "url": "/portfolio/static/media/telegram.7a1f1852.svg"
+  },
+  {
+    "revision": "f29a2e6a3b02b3856e8a53736adf6aad",
+    "url": "/portfolio/static/media/github.f29a2e6a.svg"
+  },
+  {
+    "revision": "d238b902bb772249ad0234f29b81072a",
+    "url": "/portfolio/static/media/mail.d238b902.svg"
+  },
+  {
+    "revision": "0a46be51e103734e477cb1b1d4737880",
+    "url": "/portfolio/static/media/linkedin.0a46be51.svg"
+  },
+  {
+    "revision": "37dda5f96b4313d06daecbcfc19bdc43",
+    "url": "/portfolio/index.html"
+  }
+];
